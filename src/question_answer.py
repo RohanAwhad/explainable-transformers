d1d26ef030c46e264c982a293ef68474ebbfd857
Rohan Awhad <rohan.awhad@quantiphi.com>
2021-12-24 15:25:35 +0530

    QA Explainer added

---
+++ src/question_answer.py
@@ -0,0 +1,44 @@
+import transformers
+import torch
+import shap
+
+
+class QAExplainer:
+    def __init__(self, model_path):
+        # load the model and tokenizer
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            model_path, use_fast=True
+        )
+        self.model = transformers.AutoModelForQuestionAnswering.from_pretrained(
+            model_path
+        )
+
+        self.explainer_start = shap.Explainer(
+            self._start_shap_func, self.tokenizer
+        )
+        self.explainer_end = shap.Explainer(self._end_shap_func, self.tokenizer)
+
+    def explain(self, inputs):
+        return {
+            "start_shap_values": self.explainer_start(inputs),
+            "end_shap_values": self.explainer_end(inputs),
+        }
+
+    def _shap_func(self, inputs, start):
+        """Processing of inputs to generate logits, that are ingested by shap"""
+        outs = []
+        for data in inputs:
+            question, context = data.split("[SEP]")
+            d = self.tokenizer(question, context)
+            out = self.model.forward(
+                **{k: torch.tensor(d[k]).reshape(1, -1) for k in d}
+            )
+            logits = out.start_logits if start else out.end_logits
+            outs.append(logits.reshape(-1).detach().numpy())
+        return outs
+
+    def _start_shap_func(self, inputs):
+        return self._shap_func(inputs, True)
+
+    def _end_shap_func(self, inputs):
+        return self._shap_func(inputs, False)
