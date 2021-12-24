bef463ff139c5f590c5f7dc2278afda7c5e93052
Rohan Awhad <rohan.awhad@quantiphi.com>
2021-12-24 15:46:13 +0530

    tested QA Explainer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """main.py"""
 
 from src import text_classification
+from src import question_answer
 
+"""
 model_path = "nateraw/bert-base-uncased-emotion"
 task = "text-classification"
 input_text = [
@@ -11,7 +13,12 @@ input_text = [
         " around someone who cares and is awake"
     ),
 ]
-
 explainer = text_classification.get_explainer(model_path)
 shap_values = explainer(input_text)
 print(shap_values)
+"""
+
+model_path = "distilbert-base-cased-distilled-squad"
+inputs = ["Where do I live?[SEP]My name is Sarah and I live in London"]
+qa_explainer = question_answer.QAExplainer(model_path)
+print(qa_explainer.explain(inputs))
