f3bca7e9a62aba169836db33906fb5a855403320
Rohan Awhad <rohan.awhad@quantiphi.com>
2021-12-28 12:05:12 +0530

    added output names

--- src/question_answer.py
+++ src/question_answer.py
@@ -9,13 +9,6 @@ class QAExplainer:
     """Explains QA models using SHAP"""
 
     def __init__(self, model_path):
-        self._setup_explainer(model_path)
-
-    def _setup_explainer(self, model_path):
-        """
-        Loads model and tokenizer and initializes SHAP Explainer
-        """
-        self.model_path = model_path
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(
             model_path, use_fast=True
         )
@@ -23,19 +16,20 @@ class QAExplainer:
             model_path
         )
 
+        self._start_shap_func.__func__.output_names = self._out_names
+        self._end_shap_func.__func__.output_names = self._out_names
+
         self.explainer_start = shap.Explainer(
-            self._start_shap_func, self.tokenizer
+            self._start_shap_func,
+            self.tokenizer,
         )
-        self.explainer_end = shap.Explainer(self._end_shap_func, self.tokenizer)
+        self.explainer_end = shap.Explainer(
+            self._end_shap_func,
+            self.tokenizer,
+        )
 
-    def explain(self, inputs: List[str], model_path: str = None):
-        """
-        Explains predictions of inputs using SHAP
-        and the model from model_path provided
-        """
-        if model_path and self.model_path != model_path:
-            self._setup_explainer(model_path)
-
+    def explain(self, inputs: List[str]):
+        """Explains predictions of inputs using SHAP and the model provided"""
         return {
             "start_shap_values": self.explainer_start(inputs),
             "end_shap_values": self.explainer_end(inputs),
@@ -60,3 +54,8 @@ class QAExplainer:
 
     def _end_shap_func(self, inputs):
         return self._shap_func(inputs, False)
+
+    def _out_names(self, inputs):
+        question, context = inputs.split("[SEP]")
+        d = self.tokenizer(question, context)
+        return [self.tokenizer.decode([id]) for id in d["input_ids"]]
