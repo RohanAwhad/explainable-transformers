0026d39802ec76a766eaaab05aab72a9609b02b4
Rohan Awhad <rohan.awhad@quantiphi.com>
2021-12-28 11:14:08 +0530

    Renders QA plots

--- main.py
+++ main.py
@@ -26,20 +26,31 @@ def get_explainer(task, model_path):
 
 st.title("Explainable Transformers")
 
-with st.form("user-inputs"):
-    st.write("Input your arguments")
-    model_path = st.text_input(
-        "HuggingFace Model Path", "nateraw/bert-base-uncased-emotion"
-    )
-    task = st.selectbox("Task", ["Text Classification", "Question Answering"])
+# with st.form("user-inputs"):
+st.write("Input your arguments")
+model_path = st.text_input(
+    "HuggingFace Model Path", "nateraw/bert-base-uncased-emotion"
+)
+task = st.selectbox("Task", ["Text Classification", "Question Answering"])
+if task == "Text Classification":
     inputs = [st.text_area("Enter input for model")]
+elif task == "Question Answering":
+    question = st.text_area("Enter question")
+    context = st.text_area("Enter context")
+    inputs = [question + "[SEP]" + context]
 
-    submitted = st.form_submit_button("Run")
+# submitted = st.form_submit_button("Run")
+submitted = st.button("Run")
 
-    if submitted:
-        shap_values = get_explainer(task, model_path).explain(inputs)
+if submitted:
+    shap_values = get_explainer(task, model_path).explain(inputs)
 
-        if task == "Text Classification":
-            st_shap(
-                shap.plots.text(shap_values["shap_values"][0], display=False)
-            )
+    if task == "Text Classification":
+        st_shap(shap.plots.text(shap_values["shap_values"][0], display=False))
+    elif task == "Question Answering":
+        st_shap(
+            shap.plots.text(shap_values["start_shap_values"][0], display=False)
+        )
+        st_shap(
+            shap.plots.text(shap_values["end_shap_values"][0], display=False)
+        )
