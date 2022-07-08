83420bd3f5696ee79aaf8f4e7ed5db569f964785
Rohan Awhad <rohanavad007@gmail.com>
2022-07-08 22:41:53 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read().split('\n')
 
 setuptools.setup(
-    name="Explainable Transformers",
-    version="0.1.0",
+    name="explainable-transformer",
+    version="0.1.2",
     author="Rohan Awhad and Shreya Saxena",
     author_email="rohanawhad@gmail.com",
     description="Software for debugging transformers",
@@ -21,8 +19,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "explainable_transformers"},
-    packages=setuptools.find_packages(where="explainable_transformers"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.7",
-    install_requires=requirements,
+    install_requires=[
+        'numpy',
+        'shap',
+        'transformers',
+        'torch'
+    ],
 )
